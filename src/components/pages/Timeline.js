864dc3f15858241bf0dfccf79c415a38e0a7cb81
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-08-09 22:42:10 -0300

    feat: create default timeline

--- src/components/pages/Timeline.js
+++ src/components/pages/Timeline.js
@@ -1,9 +1,28 @@
+import Styled from "styled-components";
 import { Header } from "../shared/Header";
 
 export default function Timeline() {
     return (
         <>
             <Header />
+            <TimelineStyled>
+                <h1>
+                    timeline
+                </h1>
+            </TimelineStyled>
         </>
     );
 }
+
+const TimelineStyled = Styled.div`
+    width: 611px;
+    height: 100%;
+    margin-top: 160px;
+
+    &>h1 {
+        font-weight: 700;
+        font-size: 43px;
+        line-height: 64px;
+        color: #FFFFFF;
+    }
+`;
