7e28f9c3b45ca391aa85905d4f5106da6b9a06a5
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-08-19 12:03:11 -0300

    feat: share post user

--- src/components/shared/Share.js
+++ src/components/shared/Share.js
@@ -3,16 +3,17 @@ import UserContext from "../../contexts/UserContext";
 import axios from 'axios';
 import { useState, useContext, useEffect } from 'react';
 import { IoRepeatOutline } from "react-icons/io5";
+import shareModalContext from '../../contexts/shareModalContext';
 
 export function Share({ id }) {
     const { apiUrl, authorization } = useContext(UserContext);
     const [amount, setAmount] = useState(0);
+    const { setShareModal } = useContext(shareModalContext);
 
     useEffect(() => {
         const URL = `${apiUrl}/share/${id}`;
         const promise = axios.get(URL);
         promise.then((res) => {
-            console.log(res.data);
             if (res.data.amount === null || res.data.amount === undefined) {
                 setAmount(0);
             } else {
@@ -24,7 +25,7 @@ export function Share({ id }) {
     }, [apiUrl]);
 
     return (
-        <ShareStyled>
+        <ShareStyled  onClick={() => setShareModal({ status: true, postId: id })}>
             <IoRepeatOutline />
             <p>{`${amount} re-posts`}</p>
         </ShareStyled>
